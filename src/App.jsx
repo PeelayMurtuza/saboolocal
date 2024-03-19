69ca6dccc38d65f72d292432bb3d3f114d5ad6a1
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-03-20 01:41:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import Navbar from './components/Navbar'
+import Body from './components/Body'
+import Departments from './components/Departments'
+
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Body />
+
+      <div className="bg-[#f7f8fa] flex flex-col justify-center items-center ">
+<h1 className='text-2xl pt-1'> <b> Our <span>Departments</span> </b></h1>
+        
+
+        <div className="  w-full flex justify-center items-center h-screen flex-col gap-5  ">
+          <div className="flex gap-5">
+            <Departments name="Automobile Engineering" />
+            <Departments name="Civil Engineering" />
+            <Departments name="Computer Engineering" />
+            <Departments name="Electronics Engineering" />
+          </div>
+
+          <div className="flex gap-5 ">
+            <Departments name="Extc " />
+            <Departments name=" Iformation technology" />
+            <Departments name="Mechanical Engineering" />
+            <Departments name="CSE [AIML] Engineering" />
+          </div>
+
+          <div className="flex gap-5 ">
+            <Departments name="IoT Engineering" />
+            <Departments name="Basic Humanity and Science " />
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Departments.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Departments(props) {
+    return (
+        <>
+
+
+            <div className="w1   h-48  rounded-2xl p-5 flex flex-col gap-y-3  justify-center items-center hover:border-b-2 hover:shadow-2xl hover:transition  ease-in-out delay-75  ">
+                <img className=' w-36 h-18 rounded-full ' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0B5WR7CVcbOVjRZXaSA2TaKZF7P70WI5ajA&usqp=CAU" alt='book' />
+                <h1><b>{props.name}</b></h1> 
+            </div>
+        </>
+    )
+}
+
+export default Departments
